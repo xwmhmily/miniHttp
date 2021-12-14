ea27b38a58bf51fb8f9de3f0d91c34226c3dc5cb
dym <381345509@qq.com>
2021-12-14 17:47:40 +0800

    Reset

--- controller/Defi.php
+++ controller/Defi.php
@@ -7,12 +7,20 @@ class C_Defi extends Controller {
     private $m_chart;
     
     function __construct(){
-    	$this->m_protocols = $this->load('Portfolios');
+    	$this->m_portfolios = $this->load('Portfolios');
         $this->m_chart = $this->load('Chart');
         $this->m_protocols = $this->load('Protocols');
     }
 
-    public function log(){
-        return __METHOD__;
+    public function protocols(){
+        return $this->m_protocols->Select();
+    }
+
+    public function portfolios(){
+        return $this->m_portfolios->Select();
+    }
+
+    public function chart(){
+        return $this->chart->Select();
     }
 }
