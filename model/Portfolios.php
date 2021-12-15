fc953f57e9d91a77e7adb6946d43592c2c998be1
dym <381345509@qq.com>
2021-12-15 11:32:22 +0800

    New code

--- model/Portfolios.php
+++ model/Portfolios.php
@@ -8,10 +8,17 @@ class M_Portfolios extends Model {
     }
 
     public function save($slug, $portfolios){
-        $portfolios['slug']     = $slug;
-        $portfolios['add_time'] = time();
-        $portfolios['add_date'] = date('Y-m-d H:i:s');
-        return $this->Insert($portfolios);
+        $portfolios = json_decode($portfolios, true);
+        if($portfolios['data']['user_list']){
+            foreach($portfolios['data']['user_list'] as $val){
+                $val['slug']     = $slug;
+                $val['add_time'] = time();
+                $porvaltfolios['add_date'] = date('Y-m-d H:i:s');
+                $this->Insert($val);
+            }
+        }
+
+        return true;
     }
 
     // 今天是否抓取过了
