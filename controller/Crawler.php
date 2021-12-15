3c2aa0a8f7d10a235669cbca5b392ff924299f49
dym <381345509@qq.com>
2021-12-15 14:28:47 +0800

    New code

--- controller/Crawler.php
+++ controller/Crawler.php
@@ -4,13 +4,17 @@ class C_Crawler extends Controller {
 
     public function protocols(){
         $reget = $this->getParam('reget');
-        Crawler::protocols($reget);
+        go(function() use ($reget) {
+            Crawler::protocols($reget);
+        });
         return "Working hard to fetch protocols data, pls wait";
     }
 
     public function chart(){
         $reget = $this->getParam('reget');
-        Crawler::charts($reget);
+        go(function() use ($reget) {
+            Crawler::charts($reget);
+        });
         return "Working hard to fetch chart data, pls wait";
     }
 }
